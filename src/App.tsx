0308999dcdb591597447f129c6b3979a44b437a9
George <george@acemuse.com>
2024-12-04 13:37:15 +0800

    Add top right icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Box, Center, Flex, IconButton, Stack } from "@chakra-ui/react";
 
-import { SidebarIcon } from "./icons/sidebar-icons";
+import { NewChatIcon, SidebarIcon } from "./icons/sidebar-icons";
 import { Tooltip } from "./components/ui/tooltip";
 
 function App() {
   return (
     <Flex minH={"100dvh"}>
       <Box bg={"bg.muted"} w={"260px"}>
-        <Stack>
-          <Flex>
+        <Stack h={"full"} px={3} py={2}>
+          <Flex justify={"space-between"}>
             <Tooltip
               showArrow
               content="Close sidebar"
@@ -16,6 +16,11 @@ function App() {
             >
               <IconButton variant={"ghost"}>
                 <SidebarIcon fontSize={"2xl"} color={"fg.muted"} />
+              </IconButton>
+            </Tooltip>
+            <Tooltip showArrow content="New chat">
+              <IconButton variant={"ghost"}>
+                <NewChatIcon fontSize={"2xl"} color={"fg.muted"} />
               </IconButton>
             </Tooltip>
           </Flex>
