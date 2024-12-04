377552b828d71e7a466ae350bda269518bceddd2
George <george@acemuse.com>
2024-12-04 13:31:52 +0800

    Add sidebar icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { Box, Center, Flex, Stack } from "@chakra-ui/react";
+import { Box, Center, Flex, IconButton, Stack } from "@chakra-ui/react";
+
+import { SidebarIcon } from "./icons/sidebar-icons";
 
 function App() {
   return (
     <Flex minH={"100dvh"}>
       <Box bg={"bg.muted"} w={"260px"}>
-        Side panel
+        <Stack>
+          <Flex>
+            <IconButton variant={"ghost"}>
+              <SidebarIcon fontSize={"2xl"} color={"fg.subtle"} />
+            </IconButton>
+          </Flex>
+        </Stack>
       </Box>
       <Box>
         <Stack h="full">
