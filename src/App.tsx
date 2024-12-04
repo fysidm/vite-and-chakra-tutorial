122d60f8f09d3c19f4989810bca8c87056155c70
George <george@acemuse.com>
2024-12-04 13:48:10 +0800

    Add two link on side panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
-import { Box, Center, Flex, IconButton, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Circle,
+  Flex,
+  HStack,
+  IconButton,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 
-import { NewChatIcon, SidebarIcon } from "./icons/sidebar-icons";
+import {
+  ExploreGPTIcon,
+  NewChatIcon,
+  SidebarIcon,
+  SmallGPTIcon,
+} from "./icons/sidebar-icons";
 import { Tooltip } from "./components/ui/tooltip";
 
 function App() {
@@ -24,6 +39,44 @@ function App() {
               </IconButton>
             </Tooltip>
           </Flex>
+          <Stack px={2} gap={0}>
+            <HStack
+              _hover={{
+                layerStyle: "fill.muted",
+                textDecor: "none",
+              }}
+              px={1}
+              h={10}
+              borderRadius={"lg"}
+              w={"100%"}
+            >
+              <Link href="#" variant={"plain"} _hover={{ textDecor: "none" }}>
+                <Circle size={6} bg={"bg"} borderWidth={"1px"}>
+                  <SmallGPTIcon fontSize={"md"} />
+                </Circle>
+                <Text fontSize={"sm"} fontWeight={"md"}>
+                  ChatGPT
+                </Text>
+              </Link>
+            </HStack>
+            <HStack
+              _hover={{
+                layerStyle: "fill.muted",
+                textDecor: "none",
+              }}
+              px={1}
+              h={10}
+              borderRadius={"lg"}
+              w={"100%"}
+            >
+              <Link href="#" variant={"plain"} _hover={{ textDecor: "none" }}>
+                <ExploreGPTIcon fontSize={"md"} />
+                <Text fontSize={"sm"} fontWeight={"md"}>
+                  Explore GPTs
+                </Text>
+              </Link>
+            </HStack>
+          </Stack>
         </Stack>
       </Box>
       <Box>
